a04545b4d75a67f016b8cc3d55eca2c0362d8c97
Kennedy Raine Maranion <kmaranion@gmail.com>
2017-03-27 15:53:03 -0500

    Add files via upload

---
+++ assets/js/todos.js
@@ -0,0 +1,28 @@
+// Check off specific todos by clicking
+$('ul').on("click","li", function (){
+	$(this).toggleClass("completed")
+});
+
+
+// Click on X to delete to do
+$('ul').on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	} );
+	event.stopPropagation();
+})
+
+// Input
+$("input[type='text'").keypress(function(event){
+	if (event.which === 13) {
+		// Grab todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		// Create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text'").fadeToggle();
+})
